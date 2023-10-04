46f10b44e8e0f568f9f2ad0002613c73f39d1064
phani1269 <veldurph@nexturn.com>
2023-10-04 16:17:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
+    console.log(`started on ${process.env.PORT}`);
+})
+
+app.use(express.json());
+
+app.get('/get', (req, res) => {
+    res.send("success").status(200)
+})
+
+app.post('/post', (req, res, err) => {
+    const firstName = req.body.firstName;
+    const LastName = req.body.LastName;
+    const response = {
+        "Name": firstName + LastName
+    };
+    res.send(response).status(200);
+})
+
